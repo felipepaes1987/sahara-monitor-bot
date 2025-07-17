94af5648446fad5a4c458e1d097e620480b92c64
felipepaes1987 <felipepaes1987@gmail.com>
2025-07-17 11:52:00 -0300

    Delete utils directory

--- utils/binance_fetch.py
+++
@@ -1,7 +0,0 @@
-import requests
-
-def buscar_dados_binance():
-    url = "https://api.binance.com/api/v3/klines?symbol=SAHARAUSDT&interval=15m&limit=20"
-    r = requests.get(url)
-    dados = r.json()
-    return dados
